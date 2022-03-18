0b1a8a8f74e36dfbe80e102d921f7b559a4d332f
YoonChangho <ho9938@unist.ac.kr>
2022-03-19 03:07:19 +0900

    threads/thread.c: add sleep_list

--- project1/pintos/src/threads/thread.c
+++ project1/pintos/src/threads/thread.c
@@ -24,6 +24,10 @@
    that are ready to run but not actually running. */
 static struct list ready_list;
 
+/* List of sleeping processes. They are in THREAD_BLOCKED,
+   that is, processes that are waiting for timer interrupt. */
+static struct list sleep_list;
+
 /* List of all processes.  Processes are added to this list
    when they are first scheduled and removed when they exit. */
 static struct list all_list;
@@ -91,6 +95,7 @@ thread_init (void)
 
   lock_init (&tid_lock);
   list_init (&ready_list);
+  list_init (&sleep_list);
   list_init (&all_list);
 
   /* Set up a thread structure for the running thread. */
