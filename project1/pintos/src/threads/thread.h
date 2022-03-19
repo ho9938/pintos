3fb3cb110b222754b20477e3fd9a7f1edd3b0ca0
YoonChangho <ho9938@unist.ac.kr>
2022-03-20 03:46:05 +0900

    threads/thread.h: add prototype for thread_sleep(), thread_awake()

--- project1/pintos/src/threads/thread.h
+++ project1/pintos/src/threads/thread.h
@@ -122,6 +122,9 @@ tid_t thread_create (const char *name, int priority, thread_func *, void *);
 void thread_block (void);
 void thread_unblock (struct thread *);
 
+void thread_sleep (int64_t);
+void thread_awake (int64_t);
+
 struct thread *thread_current (void);
 tid_t thread_tid (void);
 const char *thread_name (void);
